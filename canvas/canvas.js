58e2cd2325bd020a1c961b862008850aa76fdf9f
Benoit <benoit@veo.co>
2017-08-31 21:36:37 +0200

    some modif

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -1,18 +1,18 @@
 window.onload = function()
 {
     var canvas = document.getElementById('mon_canvas');
-        if(!canvas)
+    if(!canvas)
         {
-            alert("Impossible de récupérer le canvas");
-            return;
-        }
+        alert("Impossible de récupérer le canvas");
+        return;
+    }
 
     var ctx = canvas.getContext('2d');
-        if(!ctx)
+    if(!ctx)
         {
-            alert("Impossible de récupérer le context du canvas");
-            return;
-        }
+        alert("Impossible de récupérer le context du canvas");
+        return;
+    }
 
 
     document.ctx = ctx;
@@ -20,57 +20,67 @@ window.onload = function()
     function draw() {
         ctx = document.ctx;
         for (var i = 0; i < 3; i++) {
-          for (var j = 0; j < 3; j++) {
-            ctx.save();
-            ctx.fillStyle = 'rgb(' + (51 * i) + ', ' + (255 - 51 * i) + ', 255)';
-            ctx.translate(10 + j * 50, 10 + i * 50);
-            ctx.fillRect(0, 0, 25, 25);
-            ctx.restore();
-          }
+            for (var j = 0; j < 3; j++) {
+                ctx.save();
+                ctx.fillStyle = 'rgb(' + (51 * i) + ', ' + (255 - 51 * i) + ', 255)';
+                ctx.translate(10 + j * 50, 10 + i * 50);
+                ctx.fillRect(0, 0, 25, 25);
+                ctx.restore();
+            }
         }
-      }
+    }
 
-      var ball = {
+    var ball = {
         x: 100,
         y: 100,
         radius: 25,
         color: 'blue',
         draw: function() {
-          ctx.beginPath();
-          ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-          ctx.closePath();
-          ctx.fillStyle = this.color;
-          ctx.fill();
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.fillStyle = this.color;
+            ctx.fill();
         }
-      };
+    };
 
-      ctx_width = 500;
-      ctx_height = 500;
-      ctx.translate(ctx_width/2, ctx_height/2);
+    var rectangle = {
+        width:100,
+        height: 200,
+        draw: function() {
+            // console.log(' ' + this.x + ' ' + this.y + ' ' + this.width + ' ' + this.height);
+            ctx.fillStyle = 'rgba(32, 179, 32, 1)';
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    };
 
-      width = 100;
-      height = 200;
-      position = 100;
+    // INIT ==========================================
+    // on centre les coordonnées
+    ctx.translate(canvas.width/2, canvas.height/2);
+    rectangle.x = - rectangle.width/2;
+    rectangle.y = - rectangle.height/2;
 
-      function animate() {
+    var width = 100;
+    var height = 200;
+    var position = 100;
+    var raf;
+
+    function animate() {
 
         // ctx.clearRect(-ctx_width/2, -ctx_height/2, 500,500);
-        // creation d un effet de degrader
+        // creation d un effet de degrader.
+        // on n´efface pas tout, on fait un degradé par étape.
         ctx.fillStyle = 'rgba(255, 255, 255, 0.4)';
         ctx.fillRect(-canvas.width/2, -canvas.height/2, canvas.width, canvas.height);
 
         // Rotate a Rectangle
-        ctx.fillStyle = 'rgba(32, 179, 32, 1)';
-        ctx.fillRect(-width/2, -height/2, width, height);
+        rectangle.draw();
+        ball.draw();
+
         ctx.rotate(Math.PI/15);
 
-        console.log('position: ' + position );
-
-        raf =window.requestAnimationFrame(animate);
-      }
-
-
-    var raf;
+        raf = window.requestAnimationFrame(animate);
+    }
 
     canvas.addEventListener('mouseover', function(e) {
         console.log('je suis dans le canvas');
@@ -84,10 +94,5 @@ window.onload = function()
 
         window.cancelAnimationFrame(raf);
     });
-
-
-    //   setInterval(animate, 1000);
-    // animate();
-
 
 }
