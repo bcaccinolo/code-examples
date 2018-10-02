b0e49b26dec5fc99b72f5dbaf54ed0a2bf9c0653
Benoit Caccinolo <bcaccinolo@synbioz.com>
2018-10-02 09:34:06 +0200

    feat: Jurisdiction.all

--- elixir/ecto-jurisdictions/juris/lib/juris/jurisdiction.ex
+++ elixir/ecto-jurisdictions/juris/lib/juris/jurisdiction.ex
@@ -1,5 +1,7 @@
 defmodule Jurisdiction do
   use Ecto.Schema
+  # Imports only from/2 of Ecto.Query
+  import Ecto.Query, only: [from: 2]
 
   # weather is the DB table
   schema "jurisdictions" do
@@ -7,10 +9,18 @@ defmodule Jurisdiction do
     field(:name, :string)
   end
 
-def changeset(jurisdiction, params \\ %{}) do
-  jurisdiction
-  |> Ecto.Changeset.cast(params, [:code])
-  |> Ecto.Changeset.validate_required([:code])
-end
+  # Query all the Jurisdicnions
+  def all do
+    query = from( j in Jurisdiction,
+                  select: [j.name, j.code] )
+    Juris.Repo.all(query)
+  end
+
+  # Not used yet
+  def changeset(jurisdiction, params \\ %{}) do
+    jurisdiction
+    |> Ecto.Changeset.cast(params, [:code])
+    |> Ecto.Changeset.validate_required([:code])
+  end
 
 end
