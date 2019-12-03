b451f180bbce953f260f0b7dfdadfdf3a8514254
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-12-03 11:10:03 +0100

    40 java wip: Main class to create some users

---
+++ java/40-jpa-test/src/main/java/net/codejava/hibernate/Main.java
@@ -0,0 +1,26 @@
+package net.codejava.hibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("UsersDB");
+        EntityManager entityManager = factory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        User newUser = new User();
+        newUser.setEmail("billjoy@gmail.com");
+        newUser.setFullname("bill Joy");
+        newUser.setPassword("billi");
+
+        entityManager.persist(newUser);
+
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        factory.close();
+    }
+}
