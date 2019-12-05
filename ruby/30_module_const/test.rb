07f738bc25c0f190b980044f12f5681b8e60dd27
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-12-05 20:07:37 +0100

    ruby: some module testing

--- ruby/30_module_const/test.rb
+++ ruby/30_module_const/test.rb
@@ -1,15 +1,29 @@
-module Test
+# frozen_string_literal: true
 
-  CCC = "const"
+module Test
+  CCC = 'const'
 
   class << self
-  def call
-    p "calling"
+    def call
+      p 'calling'
+    end
+  end
+
+  def inst_call
+    p 'instance calling'
+  end
+
+end
+
+p Test::CCC #> 'const'
+
+Test.call #> calling
+
+Test.inst_call #> ERROR!
+
+
+module Test
+  def inst_call
+    p 'instance calling'
   end
 end
-
-end
-
-p Test::CCC
-Test.call
-
