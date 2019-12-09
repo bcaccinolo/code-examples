6793e772a93815e8bbf9a053dda81293343391ad
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-12-09 15:50:58 +0100

    50 java spring html : Running app 🚀

--- java/50-spring-boot-serve-html/genHtml/src/main/java/org/qbeek/App.java
+++ java/50-spring-boot-serve-html/genHtml/src/main/java/org/qbeek/App.java
@@ -1,13 +1,18 @@
 package org.qbeek;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello WWorld!" );
+        SpringApplication.run(App.class, args);
     }
 }
