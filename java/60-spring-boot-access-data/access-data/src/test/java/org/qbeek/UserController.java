dd11227e164f2b4c208895fb9c723f2e31eca93d
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-12-10 17:26:17 +0100

    60 java spring access data : creation of UserController

---
+++ java/60-spring-boot-access-data/access-data/src/test/java/org/qbeek/UserController.java
@@ -0,0 +1,32 @@
+package org.qbeek;
+
+import org.qbeek.accessingdata.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(name = "/demo")
+public class UserController {
+    @Autowired // will automatically instantiate the repository from the interface
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        userRepository.save(user);
+
+        return "Saved";
+    }
+
+    @GetMapping(path = "all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
+
