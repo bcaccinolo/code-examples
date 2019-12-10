79fd78b3965df9b6b0472e49b34b26a104159b52
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-12-10 17:33:35 +0100

    60 java spring access data : refacto class location

--- java/60-spring-boot-access-data/access-data/src/main/java/org/qbeek/accessingdata/UserController.java
+++ java/60-spring-boot-access-data/access-data/src/main/java/org/qbeek/accessingdata/UserController.java
@@ -1,6 +1,6 @@
-package org.qbeek;
+package org.qbeek.accessingdata;
 
-import org.qbeek.accessingdata.UserRepository;
+import org.qbeek.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
