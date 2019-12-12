43a5f9e9951b0070e3abf93e7ae75fafd6b01b30
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-12-12 18:34:07 +0100

    60 java spring access data : first attempt to test the controller

---
+++ java/60-spring-boot-access-data/access-data/src/test/java/org/qbeek/UserControllerTest.java
@@ -0,0 +1,15 @@
+package org.qbeek;
+
+import org.junit.jupiter.api.Test;
+
+public class UserControllerTest {
+
+    @Test
+    void testGetAllUsers() {
+        UserController controller = new UserController();
+        controller.getAllUsers();
+//        assertEquals();
+//        System.out.println(list);
+//        System.out.print("coucou");
+    }
+}
