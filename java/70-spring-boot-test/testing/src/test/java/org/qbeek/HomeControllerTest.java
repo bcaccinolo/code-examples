16ff52b56da9834f5650e4000c1e83abdd4910f0
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-12-13 13:07:23 +0100

    70 spring boot test : real controller test

--- java/70-spring-boot-test/testing/src/test/java/org/qbeek/HomeControllerTest.java
+++ java/70-spring-boot-test/testing/src/test/java/org/qbeek/HomeControllerTest.java
@@ -1,22 +1,34 @@
 package org.qbeek;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class HomeControllerTest {
 
     @Autowired
-    HomeController controller;
+    private MockMvc mockMvc;
 
     @Test
-    public void contextLoads() {
-        assertThat(controller).isNotNull();
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/hello")).
+                andDo(print()).
+                andExpect(status().isOk()).
+                andExpect(content().string(containsString("Hello World!")));
     }
 }
+
