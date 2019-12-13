269bf7e1d6f508a8c03998e1d7a55c9c265c3dea
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-12-13 11:35:45 +0100

    70 spring boot test : `/hello` 🚀

---
+++ java/70-spring-boot-test/testing/src/main/java/org/qbeek/HomeController.java
@@ -0,0 +1,15 @@
+package org.qbeek;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/hello")
+    public @ResponseBody String hello() {
+        return "Hello world!";
+    }
+
+}
---
+++ java/README.md
@@ -0,0 +1,5 @@
+
+
+# Important project files
+cp ~/dev/code-examples/java/60-spring-boot-access-data/access-data/README.md .
+cp ~/dev/code-examples/java/60-spring-boot-access-data/access-data/.gitignore .
