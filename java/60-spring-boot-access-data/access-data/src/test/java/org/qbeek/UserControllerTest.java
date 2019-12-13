57e7b62ca74022efe4363f3c8d7cc0f5f01c796c
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-12-13 11:00:22 +0100

    60 java spring access data : ❌ spring-boot-starter-test not working

--- java/60-spring-boot-access-data/access-data/src/test/java/org/qbeek/UserControllerTest.java
+++ java/60-spring-boot-access-data/access-data/src/test/java/org/qbeek/UserControllerTest.java
@@ -1,15 +1,26 @@
 package org.qbeek;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
 public class UserControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    void testGetAllUsers() {
-        UserController controller = new UserController();
-        controller.getAllUsers();
-//        assertEquals();
-//        System.out.println(list);
-//        System.out.print("coucou");
+    public void listUsers() throws Exception {
+        mvc.perform(get("/demo/all").accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk());
     }
 }
