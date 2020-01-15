de6f2ee0bba353c98fe061a450960228050320c8
Benoit Caccinolo <bcaccinolo@synbioz.com>
2020-01-15 16:35:30 +0100

    java renaming project directories

---
+++ java/12-with-maven-archetype/jpa-hibernate-archetype/target/test-classes/projects/basic/project/basic/src/main/java/it/pkg/Row.java
@@ -0,0 +1,28 @@
+package it.pkg;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Row {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String value;
+
+    /**
+     * Empty constructor needed for JPA.
+     */
+    @SuppressWarnings("unused")
+    public Row() {
+    }
+
+    public Row(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
