c6020acd356a3c70a14b58c07d1220c6bbe85038
Benoit Caccinolo <bcaccinolo@synbioz.com>
2020-01-21 17:17:59 +0100

    34 db authentication : wip

--- java/34-spring-security-with-db/README.md
+++ java/34-spring-security-with-db/README.md
@@ -2,5 +2,64 @@
 ## Note
 authentification a partir des users dans la base de données
 
+## Todo 
+
+- ok install flyway
+- >> install 1er migration users ...
+- install des datas
+
+## Run from command line 
+
+`
+./mvnw spring-boot:run
+`
+
+## DB configuration
+
+Let's get a db
+`
+docker pull mysql:8
+`
+
+### set db settings
+
+All the db env variables are stored in a `.env` file.
+
+### Let's run the db
+`
+docker run -d --rm --name $DB_CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=$DB_NAME mysql:8
+`
+
+### Get ip to connect
+
+Once the container is started, the `.env` file exports the IP of the container in `DB_IP`
+
+Validate 
+`
+env | sort | ack DB_
+`
+
+You should have something like this 
+`
+DB_CONTAINER_NAME=db-test-java
+DB_IP=172.18.0.3
+DB_NAME=db_example
+`
+
+### Stop the db container
+`
+docker container stop $DB_CONTAINER_NAME
+`
+
+### Connect to the db
+`
+ mycli -u root -p root -h $DB_IP $DB_NAME
+`
+
+### Flyway : Fill the db 
+`
+flyway -configFiles=src/main/resources/application.properties migrate
+`
+
 
 
---
+++ java/34-spring-security-with-db/src/main/java/com/example/demo/config/WebSecurity.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource);
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
