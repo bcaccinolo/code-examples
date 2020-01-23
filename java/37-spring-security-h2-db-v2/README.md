cd44885735923201c7b2d675fdccf97eec5ca505
Benoit Caccinolo <bcaccinolo@synbioz.com>
2020-01-23 15:43:12 +0100

    37 spring security jdbc h2 V2 : wip

---
+++ java/37-spring-security-h2-db-v2/README.md
@@ -0,0 +1,6 @@
+## source 
+https://www.baeldung.com/spring-security-jdbc-authentication
+
+## Retour
+
+on y croit !
---
+++ java/37-spring-security-h2-db-v2/src/test/java/com/example/springsecurityh2dbv2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityh2dbv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
