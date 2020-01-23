9fc38f7ba3e1ceed7cd495b6ce22fbdfb2160262
Benoit Caccinolo <bcaccinolo@synbioz.com>
2020-01-23 14:13:13 +0100

    35 spring security jdbc h2 : add the security config

---
+++ java/35-spring-security-h2-db/src/main/java/com/example/springsecurityh2db/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.springsecurityh2db;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class SecurityConfig implements WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .withDefaultSchema()
+                .dataSource(dataSource)
+                .withUser(User.withUsername("benoit").password("root").roles("USER"));
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
