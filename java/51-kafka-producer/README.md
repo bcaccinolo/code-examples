2dc4d29c244dfe3a4ad9a0a295b296cd2c28f945
Benoit C <benoit.caccinolo@gmail.com>
2020-03-16 12:53:40 +0100

    51 - send a message

--- java/51-kafka-producer/README.md
+++ java/51-kafka-producer/README.md
@@ -1,5 +1,13 @@
 
 
+
+# Launch Zookeeper & Kafka
+
+https://hub.docker.com/r/confluent/kafka/
+
+
+docker run --name kafka -p 9092:9092 --link zookeeper:zookeeper confluent/kafka
+
 # Launch the app
 
 java -cp target/kafka-producer-1.0-SNAPSHOT.jar org.qbeek.App
--- java/51-kafka-producer/src/main/java/org/qbeek/App.java
+++ java/51-kafka-producer/src/main/java/org/qbeek/App.java
@@ -1,13 +1,37 @@
 package org.qbeek;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.kafka.clients.Metadata;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Properties;
+
+public class App {
+
+    // instantiate a producer
+    private static Properties kafkaProps = new Properties();
+    // creation of a Producer Record
+    static ProducerRecord<String, String> record = new ProducerRecord<>("test", "Precision", "France");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        kafkaProps.put("bootstrap.servers", "localhost:9092");
+        kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(kafkaProps);
+
+        try {
+            System.out.println("Sending a record ...");
+            RecordMetadata metadata = producer.send(record).get();
+            System.out.println(metadata);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Done");
+
+
     }
 }
