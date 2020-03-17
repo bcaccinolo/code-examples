cc47f90a3cd15125f840423a2d56bf6c136386eb
Benoit C <benoit.caccinolo@gmail.com>
2020-03-17 09:34:54 +0100

    52 kafka consumer: initial commit

---
+++ java/52-kafka-consumer/src/main/java/org/qbeek/App.java
@@ -0,0 +1,13 @@
+package org.qbeek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        MyConsumer.launch();
+    }
+}
---
+++ java/52-kafka-consumer/src/main/java/org/qbeek/MyConsumer.java
@@ -0,0 +1,9 @@
+package org.qbeek;
+
+public class MyConsumer {
+
+    public static void launch() {
+        System.out.println("Launching consumer...");
+    }
+
+}
