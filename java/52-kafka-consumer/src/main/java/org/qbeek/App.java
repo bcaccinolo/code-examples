4ad5604e77b3829f4ac6820ae5509fcd0810033a
Benoit C <benoit.caccinolo@gmail.com>
2020-03-17 16:30:23 +0100

    52 kafka consumer: send message

--- java/52-kafka-consumer/src/main/java/org/qbeek/App.java
+++ java/52-kafka-consumer/src/main/java/org/qbeek/App.java
@@ -1,5 +1,7 @@
 package org.qbeek;
 
+import java.util.Properties;
+
 /**
  * Hello world!
  *
@@ -10,4 +12,5 @@ public class App
     {
         MyConsumer.launch();
     }
+
 }
--- java/52-kafka-consumer/src/main/java/org/qbeek/MyConsumer.java
+++ java/52-kafka-consumer/src/main/java/org/qbeek/MyConsumer.java
@@ -1,9 +1,44 @@
 package org.qbeek;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Collections;
+import java.util.Properties;
+
 public class MyConsumer {
 
     public static void launch() {
         System.out.println("Launching consumer...");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties());
+        consumer.subscribe(Collections.singleton("test"));
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(100);
+                for (ConsumerRecord<String, String> record : records)
+                    System.out.println(
+                            String.format("topic = %s, partition = %s, offset = %d, customer = %s, country = %s\n",
+                                    record.topic(), record.partition(), record.offset(),
+                                    record.key(), record.value()));
+            }
+        } finally {
+            consumer.close();
+        }
     }
+
+    private static Properties properties() {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("group.id", "CountryCounter");
+        properties.put("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        return properties;
+    }
+
 
 }
