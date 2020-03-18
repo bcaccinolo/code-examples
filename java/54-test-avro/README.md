c1998b8af8a972e72b6c34abce00dd82b81d3102
Benoit C <benoit.caccinolo@gmail.com>
2020-03-18 16:58:36 +0100

    54 Avro - first encoding in binary

--- java/54-test-avro/README.md
+++ java/54-test-avro/README.md
@@ -1,25 +1,6 @@
 
-Here we have a Kafka producer sending messages every x sec.
+Here we have a Avro serializer
 
-# Launch Zookeeper & Kafka
+## TODO
 
-cd /home/benoit/dev/kafka_2.12-2.4.1
-
-### Zookeeper
-bin/zookeeper-server-start.sh config/zookeeper.properties
-
-### Kafka
-bin/kafka-server-start.sh config/server.properties
-
-### Create a topic 
-bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test
-
-### Producer
-bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test
-
-### Consumer 
-bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic test --from-beginning
-
-# Launch the app
-
-java -cp target/kafka-producer-1.0-SNAPSHOT.jar org.qbeek.App
+- Read from a resources file.
--- java/54-test-avro/src/main/java/org/qbeek/App.java
+++ java/54-test-avro/src/main/java/org/qbeek/App.java
@@ -1,13 +1,55 @@
 package org.qbeek;
 
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumWriter;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        File schemaFile = new File(classLoader.getResource("user.avsc").getFile());
+        System.out.println(String.format("The schemaFile path %s", schemaFile));
+
+        try {
+            Schema schema = new Schema.Parser().parse(schemaFile);
+            System.out.println(schema);
+
+            GenericRecord user1 = new GenericData.Record(schema);
+            user1.put("name", "Alyssa");
+            user1.put("favorite_number", 256);
+            // Leave favorite color null
+
+            GenericRecord user2 = new GenericData.Record(schema);
+            user2.put("name", "Ben");
+            user2.put("favorite_number", 7);
+            user2.put("favorite_color", "red");
+
+            System.out.println(user1);
+
+            // Serialize user1 and user2 to disk
+            File file = new File("users.avro");
+            DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<GenericRecord>(schema);
+            DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<GenericRecord>(datumWriter);
+            dataFileWriter.create(schema, file);
+            dataFileWriter.append(user1);
+            dataFileWriter.append(user2);
+            dataFileWriter.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
