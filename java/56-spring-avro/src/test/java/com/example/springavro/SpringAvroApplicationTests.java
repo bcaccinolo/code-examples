cf2c4988685b1fedf5dd4dffc644c8d46e02ae87
Benoit Caccinolo <benoit.caccinolo@gmail.com>
2020-03-23 14:42:32 +0100

    56 spring avro - initial commit

---
+++ java/56-spring-avro/src/test/java/com/example/springavro/SpringAvroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springavro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAvroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
