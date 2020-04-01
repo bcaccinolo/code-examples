b190d4782c07d1bbb6322bb132fa26cacedf5045
Benoit C <benoit.caccinolo@gmail.com>
2020-04-01 10:42:55 +0200

    13 jpa test - add Lombok

--- java/13-jpa-test/src/main/java/net/codejava/hibernate/User.java
+++ java/13-jpa-test/src/main/java/net/codejava/hibernate/User.java
@@ -1,5 +1,7 @@
 package net.codejava.hibernate;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 /**
@@ -8,45 +10,15 @@ import javax.persistence.*;
  */
 @Entity
 @Table(name = "users")
+@Data
 public class User {
 
-    @Column(name = "USER_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Id private Integer id;
+    @Id
+    private Integer id;
 
     private String fullname;
     private String email;
     private String password;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private String coucou;
 }
